5df5b90d8db6a430a7fb9185c1b261e6e9a40e91
Anthony Tran <quanghust96@gmail.com>
2022-12-07 01:37:50 +0700

    tao moi project

---
+++ JAVASCRIPT-HANGMAN-GAME.js
@@ -0,0 +1,12 @@
+// categories = [
+//     ["everton", "liverpool", "swansea", "chelsea", "hull", "manchester-city", "newcastle-united"],
+//     ["alien", "dirty-harry", "gladiator", "finding-nemo", "jaws"],
+//     ["manchester", "milan", "madrid", "amsterdam", "prague"]
+// ];
+
+// hints = [
+//     ["Based in Mersyside", "Based in Mersyside", "First Welsh team to reach the Premier Leauge", "Owned by A russian Billionaire", "Once managed by Phil Brown", "2013 FA Cup runners up", "Gazza's first club"],
+//     ["Science-Fiction horror film", "1971 American action film", "Historical drama", "Anamated Fish", "Giant great white shark"],
+//     ["Northern city in the UK", "Home of AC and Inter", "Spanish capital", "Netherlands capital", "Czech Republic capital"]
+// ];
+
